8e4b32cc1762b10dafab5f0aa8f58233bef7454c
ignis <maildevcom+1@gmail.com>
2025-09-09 13:42:23 -0400

    feat: avatar component;

--- src/index.ts
+++ src/index.ts
@@ -29,11 +29,13 @@ export { ForgeProgress } from './components/atoms/progress/progress';
 export { ForgeProgressCircle } from './components/atoms/progress/progress-circle';
 export { ForgeSkeleton } from './components/atoms/skeleton/skeleton';
 export { ForgeAspectRatio } from './components/atoms/aspect-ratio/aspect-ratio';
+export { ForgeAvatar, type AvatarSize, type AvatarStatus, type StatusPosition, type AvatarShape } from './components/atoms/avatar/avatar';
 
 // Molecule exports
 export { ForgeFormField, type FormFieldVariant, type FormFieldValidationState } from './components/molecules/form-field/form-field';
 export { ForgeMultiSelect, type MultiSelectOption } from './components/molecules/multi-select/multi-select';
 export { ForgeDatePicker, type DateRange } from './components/molecules/date-picker/date-picker';
+export { ForgeDropdown, type DropdownItem } from './components/molecules/dropdown/dropdown';
 export { ForgeCard, type CardVariant, type CardSize } from './components/molecules/card/card';
 export { ForgeModal, type ModalSize, type ModalScrollBehavior } from './components/molecules/modal/modal';
 export { ForgeToast } from './components/molecules/toast/toast';
